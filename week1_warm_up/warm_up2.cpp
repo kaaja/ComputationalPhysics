513a45a29ea20cece8e541108f21b8670afa2822
Karl Jacobsen <karljaco@gmail.com>
2017-08-25 21:19:31 +0200

    New try on warm up. Problems with initialize function.

---
+++ week1_warm_up/warm_up2.cpp
@@ -0,0 +1,74 @@
+/* Program for 2nd derivative, Heavily based on MHJ Ch 3.1*/
+
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+void initialize(double *, double *, int *);
+void second_derivative(double *, double *, double, double, int);
+void output(double *, double *, double, int);
+
+ofstream ofile;
+
+int main(int argc, char *argv[]){
+  // Declaration variables
+  char *outfilename;
+  int number_of_steps;
+  double x, initial_step;
+  double *computed_derivative, *h_step;
+  
+  if( argc <= 1){
+    cout << "Give a name to the output file after the program name" << endl;
+    exit(1);
+  }
+  else{
+    outfilename=argv[1];
+  }
+  ofile.open(outfilename);
+
+  // Read variables from command line
+  initialize (&x, &initial_step, &number_of_steps);
+
+  // Pointer/arrays
+  h_step = new double[number_of_steps];
+  computed_derivative = new double[number_of_steps];
+
+  second_derivative(computed_derivative, h_step, x, initial_step, number_of_steps);
+
+  output(h_step, computed_derivative, x, number_of_steps);
+
+  delete [] h_step;
+  delete [] computed_derivative;
+
+  ofile.close();
+  return 0;
+}
+
+void initilize(double *x, double *initial_step, int *number_of_steps){
+    /* Why star for non-pointers?*/
+  printf("Read in from screen x, initial step, and number of steps \n");
+  scanf("%lf %lf %d", initial_step, x, number_of_steps);
+  return;
+}
+
+void second_derivative(double *computed_derivative, double *h_step, double x, double initial_step, int number_of_steps ){
+  double h = initial_step;
+  for (int i = 0; i < number_of_steps; i++){
+    h_step[i] = h;
+    computed_derivative[i] = (atan(x+h) - atan(x))/h;
+    h = h*0.5;
+  }
+  return;
+}
+void output(double *computed_derivative, double *h_step, double x, int number_of_steps){
+
+  ofile << "RESULTS:" << endl;
+  ofile << setiosflags(ios::showpoint | ios::uppercase);
+  for( int i = 0; i < number_of_steps; i++){
+    ofile << setw(15) << setprecision(8) << log10(h_step[i]);
+    ofile << setw(15) << setprecision(8) << log10(fabs(computed_derivative[i] - 1./3)/(1./3)) << endl;
+  }
+}
+
