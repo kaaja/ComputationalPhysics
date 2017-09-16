69534c1a5091c80f67f5f531b50fd4b360684387
Karl Jacobsen <karljaco@gmail.com>
2017-09-16 12:45:30 +0200

    Jacobi working

--- project2/main.cpp
+++ project2/main.cpp
@@ -9,18 +9,18 @@ using namespace std;
 using namespace arma;
 
 
-void jacobi(mat A, colvec &eigenValues, double tolerance, int maxIterations, int N);
-void findMaxNonDiagonalElement(mat &B, int *k, int *l, int N);
-void computeB(mat aMatrix, mat &bMatrix, int k, int l);
-void createEigenvalueVector( mat B, colvec &eigenValues, int N );
+void jacobi(mat &A, colvec &eigenValues, double tolerance, int maxIterations, int N);
+double findMaxNonDiagonalElement(mat &A, int *k, int *l, int N);
+void rotate(mat &aMatrix, int k, int l, int N);
+void createEigenvalueVector( mat A, colvec &eigenValues, int N );
 
 
 
 int main()
 {
   int N = 3;
-  double tolerance = pow(10, -5);
-  int maxIterations = 100;
+  double tolerance = pow(10, -10);
+  int maxIterations = pow(10, 4);
 
   mat A(3,3);
   A(0,0) = 1;
@@ -37,66 +37,88 @@ int main()
   colvec eigenValues  = zeros<colvec>(N);
   jacobi(A, eigenValues, tolerance, maxIterations, N);
   eigenValues.print("Eigenvalues =");
+  A.print("A:");
   return 0;
 }
 
-void jacobi(mat A, colvec &eigenValues, double tolerance, int maxIterations, int N){
-    mat B;
-    B = A;
+void jacobi(mat &A, colvec &eigenValues, double tolerance, int maxIterations, int N){
     int k,l, counter;
     counter = 0;
 
-    findMaxNonDiagonalElement(B, &k, &l, N);
+    double aMaxNonDiagonal = findMaxNonDiagonalElement(A, &k, &l, N);
 
-    cout << "k: " << k << "l: " << l << endl;
-
-    while ( B(k, l) > tolerance && counter < maxIterations ){
-        findMaxNonDiagonalElement(B, &k, &l, N);
-        computeB(A, B, k, l);
+    while ( fabs(aMaxNonDiagonal) > tolerance && counter < maxIterations ){
+        aMaxNonDiagonal  = findMaxNonDiagonalElement(A, &k, &l, N);
+        rotate(A, k, l, N);
+        //findMaxNonDiagonalElement(A, &k, &l, N);
         counter += 1;
     }
-    createEigenvalueVector( B, eigenValues,  N);
+    cout << "max a: " << fabs(A(k, l)) << endl;
+    cout << "counter  " << counter << endl;
+    createEigenvalueVector( A, eigenValues,  N);
 }
 
-void computeB(mat aMatrix, mat &bMatrix, int k, int l){
-    double tau, t, c, s;
-
-    tau = (aMatrix(l,l) - aMatrix(k,k))/2*aMatrix(k,l);
-    t = - tau + sqrt(1 + tau*tau);
-    c = 1./sqrt(1+t*t);
-    s = t*c;
-
-    for (int i = 0; i < 2; i++){
-        if ( (i != k) && (i != l) ) {
-                bMatrix(i,i) = aMatrix(i, i);
-                bMatrix(i,k) = aMatrix(i, k)*c - aMatrix(i, l)*s;
-                bMatrix(i, l) = aMatrix(i, l)*c + aMatrix(i, k)*s;
+void rotate(mat &aMatrix, int k, int l, int N){
+    double c, s;
+    if ( aMatrix(k,l) != 0.0 ) {
+        double tau, t;
+        tau = (aMatrix(l,l) - aMatrix(k,k))/(2.*aMatrix(k,l));
+        if ( tau >= 0 ) {
+          t = 1.0/(tau + sqrt(1.0 + tau*tau));
         }
-    bMatrix(k, k) = aMatrix(k, k)*c*c - 2.*aMatrix(k, l)*c*s + aMatrix(l, l)*s*s;
-    bMatrix(l, l) = aMatrix(l, l)*c*c + 2.*aMatrix(k, l)*c*s + aMatrix(k, k)*s*s;
-    bMatrix(k,l) = 0.0;
-    bMatrix(l,k) = 0.0;
+        else {
+          t = -1.0/(-tau +sqrt(1.0 + tau*tau));
+        }
+
+        c = 1/sqrt(1+t*t);
+        s = c*t;
     }
+    else {
+        c = 1.0;
+        s = 0.0;
+    }
+
+    double aIk, aIl, aKk, aLl;
+    for (int i = 0; i < N; i++){
+        if ( i != k && i != l ) {
+                aIk = aMatrix(i, k);
+                aIl = aMatrix(i, l);
+                aMatrix(i,k) = aIk*c - aIl*s;
+                aMatrix(k, i) = aMatrix(i, k);
+                aMatrix(i, l) = aIl*c + aIk*s;
+                aMatrix(l, i) = aMatrix(i, l);
+        }
+    }
+    aKk = aMatrix(k,k);
+    aLl = aMatrix(l, l);
+    aMatrix(k, k) = aKk*c*c - 2.*aMatrix(k, l)*c*s + aLl*s*s ;
+    aMatrix(l, l) = aLl*c*c + 2.*aMatrix(k, l)*c*s + aKk*s*s;
+    aMatrix(k,l) = 0.0;
+    aMatrix(l,k) = 0.0;
 }
 
-void findMaxNonDiagonalElement(mat &B, int *k, int *l, int N){
-    *k = 0;
-    *l = 0;
+double findMaxNonDiagonalElement(mat &A, int *k, int *l, int N){
+    //*k = 0;
+    //*l = 0;
+    double aMaxTemp = 0.0;
     for (int row = 0; row < N; row++){
-         for (int col = 1; col < N; col++){
-             if( (row != col) && (B(row, col) > B(row, col -1)) ) {
+         for (int col = row + 1; col < N; col++){
+             if( row != col && fabs(A(row, col)) >  aMaxTemp ) {
                  *k = row;
                  *l = col;
+                 aMaxTemp = fabs(A(row, col));
+                 cout << "non diag max: " << aMaxTemp << endl;
              }
          }
     }
+    return aMaxTemp;
 }
 
-void createEigenvalueVector( mat B, colvec &eigenValues, int N ){
+void createEigenvalueVector( mat A, colvec &eigenValues, int N ){
     for (int row = 0; row < N; row++){
         for (int col= 0; col < N; col++){
             if ( col == row )
-                eigenValues(col) = B(row, col);
+                eigenValues(col) = A(row, col);
         }
     }
 }
