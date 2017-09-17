a547ed9d7c225cc13e23e52941360be7257ddde7
Karl Jacobsen <karljaco@gmail.com>
2017-09-17 13:22:54 +0200

    initializing, output

--- project2/main.cpp
+++ project2/main.cpp
@@ -3,6 +3,9 @@
 #include <iostream>
 #include <armadillo>
 #include <cmath>
+#include <fstream>
+#include <iomanip>
+#include "time.h"
 
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
@@ -10,21 +13,35 @@
 using namespace std;
 using namespace arma;
 
-
+void initialize(string& outfile_name, int& number_of_simulations,int& amplificationFactor, int& N, int& rhoMax,int& maxIterations, double& tolerance, int argc, char** argv );
 void jacobi(mat &A, colvec &eigenValues, double tolerance, int maxIterations, int N);
 double findMaxNonDiagonalElement(mat &A, int *k, int *l, int N);
 void rotate(mat &aMatrix, int k, int l, int N);
 void createEigenvalueVector( mat A, colvec &eigenValues, int N );
 void createTridiagonalMatrix( mat &A, int N, double rhoMax, double rhoMin);
+void output_scalars( double computedError, double h, double timeUsed, int N, int counter);
+void output_vectors( double *, int, int, string);
+void calculateError(colvec eigenValues, double *computedError);
 
+ofstream ofile1; // File for scalars
+ofstream ofile2; // File for vectors
 
 main(int argc, char* argv[]){ 
-  int result = Catch::Session().run( argc, argv );
-  int N = 600;
-  double rhoMax = pow(10,1);
-  double tolerance = pow(10, -10);
-  int maxIterations = pow(10, 6);
 
+  if (3 == 3)
+      int result = Catch::Session().run( argc, argv );
+
+  double tolerance, computedError;
+  int N, rhoMax, amplificationFactor, numberOfSimulations, maxIterations;
+  string outfileName, outfileNameComputedNumerical, outfileNameScalars;
+  clock_t start, finish;
+
+  initialize(outfileName, numberOfSimulations, amplificationFactor,N, rhoMax, maxIterations, tolerance, argc, argv );
+
+  outfileNameScalars = (outfileName) + string("_scalars")+string(".csv");
+  outfileNameComputedNumerical = (outfileName) + string("_numerical");
+  ofile1.open(outfileNameScalars);
+  ofile1 << "logH,logRelError,timeUsed,logTimeUsed,N,logN,counter,logCounter" << endl;
 
   mat A;
   createTridiagonalMatrix(A, N, rhoMax, 0);
@@ -37,9 +54,56 @@ main(int argc, char* argv[]){
   colvec eigenValues  = zeros<colvec>(N);
   jacobi(A, eigenValues, tolerance, maxIterations, N);
   eigenValues.print("Eigenvalues =");
+  calculateError(eigenValues, &computedError);
+  ofile1.close();
+  ofile2.close();
 
 
   return 0;
+}
+
+void initialize(string& outfile_name, int& number_of_simulations,int& amplificationFactor, int& N, int& rhoMax,int& maxIterations, double& tolerance, int argc, char** argv )
+{
+    if( argc<= 1){
+      cout << "Insert: outfile-name, number of simulations, amplification factor, start dimension" << endl;
+      exit(1);
+    }
+    else{
+      outfile_name=argv[1];
+    }
+    number_of_simulations = atoi(argv[2]);
+    amplificationFactor = atoi(argv[3]);
+    N = atoi(argv[4]);
+    rhoMax = atoi(argv[5]);
+    maxIterations = atoi(argv[6]);
+    tolerance = atof(argv[7]);
+}
+
+//ofile1 << "logH,logRelError,timeUsed,logTimeUsed,N,logN,counter,logCounter" << endl;
+void output_scalars( double computedError, double h, double timeUsed, int N, int counter){
+  ofile1 << setiosflags(ios::showpoint | ios::uppercase);
+  ofile1 << setw(15) << setprecision(16) << log10(h) << ", ";
+  ofile1 << setw(15) << setprecision(16) << computedError << ", ";
+  ofile1 << setw(15) << setprecision(16) << timeUsed << ", ";
+  ofile1 << setw(15) << setprecision(16) << log10(timeUsed) << ", ";
+  ofile1 << setw(15) << setprecision(16) << N << ", ";
+  ofile1 << setw(15) << setprecision(16) << log10(N) << ", ";
+  ofile1 << setw(15) << setprecision(16) << counter << ", ";
+  ofile1 << setw(15) << setprecision(16) << log10(counter) << endl;
+}
+
+void output_vectors( double *output_array, int simulation_number, int N, string outfile_name){
+  string filename = outfile_name + to_string(simulation_number+1)+string(".csv");
+  ofile2.open(filename);
+  /*
+  ofile2 << "simulation_number_" << simulation_number+1 << endl;
+  ofile2 << setiosflags(ios::showpoint | ios::uppercase);
+  for (int i = 0; i<N; i++){
+    ofile2 << setw(15) << setprecision(16) << output_array[i] << endl;;
+  }
+  ofile2 << setw(15) << setprecision(16) << endl;
+  */
+  ofile2.close();
 }
 
 void jacobi(mat &A, colvec &eigenValues, double tolerance, int maxIterations, int N){
@@ -138,11 +202,71 @@ void createTridiagonalMatrix( mat &A, int N, double rhoMax, double rhoMin){
     A(N-1, N-1) = 2.0/(h*h) + pow((N-1),2);
 }
 
-TEST_CASE( "Testing createTridiagonalMatrix", "[createTridiagonalMatrix]" ) {
-  mat A;
-  createTridiagonalMatrix(A, 10, 10, 0);
-  REQUIRE( A(0,0)== 2);
+void calculateError(colvec eigenValues, double *computedError){
+    // Calculates sup-norm for relative error of 3 lowest eigenvalues
+    colvec analyticalSolution  = zeros<colvec>(3);
+    analyticalSolution(0) = 3.0;
+    analyticalSolution(1) = 7.0;
+    analyticalSolution(2) = 11.0;
+    *computedError = 0.0;
+    double temp;
+    for (int i = 0; i < 3; i++) {
+        temp = log10(fabs((analyticalSolution(i) - eigenValues(i))/analyticalSolution(i)));
+        if (temp > *computedError)
+            *computedError = temp;
+    }
 }
+
+
+TEST_CASE( "Testing maxDiagonal", "[findMaxNonDiagonalElement]" ) {
+  // Checks that function finds max abs non-diagonal element in known symmetric matrix
+  int k, l, N;
+  N = 3;
+
+  mat A(N,N);
+  A(0,0) = 10.;
+  A(0,1) = 1.;
+  A(0,2) = -6.;
+  A(1,0) = 1.;
+  A(1,1) = 20.;
+  A(1,2) = 3.;
+  A(2,0) = -6.;
+  A(2,1) = 3.;
+  A(2,2) = 30.;
+
+  double max = findMaxNonDiagonalElement(A, &k, &l, N);
+  REQUIRE( max == 6.);
+}
+
+
+TEST_CASE( "Testing Jacobi eigenvalues", "[jacobi]" ) {
+  // Checks that function finds max abs non-diagonal element in known symmetric matrix
+  int k, l, N;
+  N = 3;
+  colvec eigenValues  = zeros<colvec>(N);
+  vec EigvalArmadillo(N);
+  double tolerance = pow(10,-10); // Tolerance for non-diagonals in Jacobi
+  int maxIterations = pow(10,4);
+  double tol = pow(10,-1); // Tolerance for difference jacobi and Armadillo
+
+  mat A(N,N);
+  A(0,0) = 10.;
+  A(0,1) = 1.;
+  A(0,2) = -6.;
+  A(1,0) = 1.;
+  A(1,1) = 20.;
+  A(1,2) = 3.;
+  A(2,0) = -6.;
+  A(2,1) = 3.;
+  A(2,2) = 30.;
+
+  eig_sym(EigvalArmadillo, A);
+  jacobi(A, eigenValues, tolerance, maxIterations, N);
+  for (int i = 0; i < N; i++){
+          REQUIRE( fabs((eigenValues(i) - EigvalArmadillo(i))/EigvalArmadillo(i)) < tol);
+  }
+}
+
 
 /*
 TEST_CASE( "Testing jacobi", "[jacobi]" ) {
