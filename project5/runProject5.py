6f24654f2ba1c2fe4641be85a2a88fb0bfce689d
Karl Jacobsen <karljaco@gmail.com>
2017-11-15 14:38:42 +0100

    solution several time steps in 1 figure

--- project5/runProject5.py
+++ project5/runProject5.py
@@ -28,59 +28,92 @@ class Project5:
         call(["./AllrunVectorized", outfileName, dt, dx, theta, T])
         
     def project5c(self):
-        dx = 0.01
-        safetyFactor = 1.1
-        dt = dx**2/2.0*(1/safetyFactor)
-        alpha = dt/dx**2
-        theta = 0.5
-        nT = 1000
-        T = (nT-1.)*dt
-        outfileName ='out5C'
-        outfileName2 = os.getcwd() + '/results/' + outfileName
-        saveEveryNSolution = 10
+        dxValues = [0.1]#, 0.01]
+        safetyFacors = [0.96, 1.04]
+        movieCounter = 0
+        for dx in dxValues:
+            for safetyFactor in safetyFacors:
+                movieCounter += 1
+                dt = dx**2/2.0*(1/safetyFactor)
+                alpha = dt/dx**2
+                theta = 0.5
+                T = 0.5
+                nT = T/dt+1
+                outfileName ='out5C'
+                outfileName2 = os.getcwd() + '/results/' + outfileName
+                saveEveryNSolution = 10
+                
+                self.runCpp(outfileName2, dt, dx, theta, T)
+                
+                # Read data
+                data = OrderedDict()
+                scenerios = ['BackwardEuler', 'ForwardEuler', 'CrancNicholson', 'Analytical']
+                x = np.zeros(int(round(1./dx+1)))
+                t = np.zeros(int(round(T/dt +1)))
+                for scenario in scenerios:    
+                    data[scenario] = pd.read_csv(outfileName2 + scenario + 'SolutionMatrixU.txt', delim_whitespace=True, header=None)
+                x = data[scenario].ix[1:,0]
+                t = data[scenario].ix[0,1:]
+                
+                #results4b.columns = ["acceptedMoves","mcs", "temperature", "Eavg", "sigmaE", "Mavg", "sigmaM", "absMavg", "Cv", "chi"]
+                
+                #Plots
+                counter =1
+                figureNumber = 1
+                fig2,ax2 = plt.subplots()
+                ax2.hold(True)
+                ax2Title = []
+                
+                for times in t:
+                    fig,ax = plt.subplots()
+                    ax.set_xlim([0, 1])
+                    ax.set_ylim([0, 1])
+                    ax.hold(True)
+                    if counter%saveEveryNSolution == 0 or counter == 1:
+                        for scenario in scenerios:
+                             ax.plot(x,data[scenario].ix[1:,counter])
+                        figureNumber += 1        
+                        ax.set_xlabel(r'$x$')
+                        ax.set_ylabel(r"$u(x,t)$")
+                        ax.set_title(r'$\Delta x = %.g $' %dx + r' $\frac{\Delta t}{\Delta x^2} = %g $ ' %alpha + '\n' + 't = %.5g' %times+ '\n')
+                        ax.legend(scenerios, loc=2)
+                        fig.tight_layout()
+                        fig.savefig('movie/%1dtmp_%04d' %(movieCounter, figureNumber) + outfileName + '.png') 
+                        plt.close()
+                    counter += 1
         
-        self.runCpp(outfileName2, dt, dx, theta, T)
+                    if counter == 2 or counter == len(t)/6 or counter == len(t):
+                        for scenario in scenerios:
+                            if scenario == 'ForwardEuler':
+                                markerType = 'r-+'
+                            elif scenario == 'BackwardEuler':
+                                markerType = 'b-o'
+                            elif scenario == 'CrancNicholson':
+                                markerType = 'g-^'
+                            else:
+                                markerType = 'y-'
+                            ax2.plot(x,data[scenario].ix[1:,counter], markerType)
+                        ax2Title.append(round(times,4))
         
-        # Read data
-        data = OrderedDict()
-        scenerios = ['BackwardEuler', 'ForwardEuler', 'CrancNicholson', 'Analytical']
-        x = np.zeros(int(round(1./dx+1)))
-        t = np.zeros(int(round(T/dt +1)))
-        for scenario in scenerios:    
-            data[scenario] = pd.read_csv(outfileName2 + scenario + 'SolutionMatrixU.txt', delim_whitespace=True, header=None)
-        x = data[scenario].ix[1:,0]
-        t = data[scenario].ix[0,1:]
-        #results4b.columns = ["acceptedMoves","mcs", "temperature", "Eavg", "sigmaE", "Mavg", "sigmaM", "absMavg", "Cv", "chi"]
-        
-        #Plots
-        counter =1
-        figureNumber = 1
-        for times in t:
-            if counter%saveEveryNSolution == 0 or counter == 1:
-                fig,ax = plt.subplots()
-                plt.hold('on')
-                for scenario in scenerios:
-                    ax.plot(x,data[scenario].ix[1:,counter])
-                ax.set_xlabel(r'$x$')
-                ax.set_ylabel(r"$u(x,t)$")
-                ax.set_title(r'$\Delta x = %.g $' %dx + r' $\frac{\Delta t}{\Delta x^2} = %g $ ' %alpha + '\n' + 't = %.5g' %times+ '\n')
-                ax.legend(scenerios, loc=2)
-                fig.tight_layout()
-                fig.savefig('movie/tmp_%04d' %figureNumber + outfileName + '.png') 
+                ax2.set_title(r'$\Delta x = %.g $' %dx + r' $\frac{\Delta t}{\Delta x^2} = %g $ ' %alpha + '\n' + 't = %s' %ax2Title + '\n')
+                ax2.legend(scenerios, loc=2)
+                ax2.set_xlabel(r'$x$')
+                ax2.set_ylabel(r"$u(x,t)$")
+                fig2.tight_layout()
+                fig2.savefig(outfileName + 'Number' + str(movieCounter) + '.png') 
                 plt.close()
-                figureNumber += 1
-            counter += 1
         
-        # Make video file
-        fps = 8  # frames per second
-        codec2ext = dict(libx264='mp4')  # video formats
-        filespec = 'movie/tmp_%04d' + outfileName + '.png'
-        movie_program = 'ffmpeg'  # or 'avconv'
-        for codec in codec2ext:
-            ext = codec2ext[codec]
-            cmd = '%(movie_program)s -r %(fps)d -i %(filespec)s ' \
-                  '-vcodec %(codec)s movie3_6.%(ext)s' % vars()
-            os.system(cmd)
+                
+#                # Make video file
+#                fps = 4  # frames per second
+#                codec2ext = dict(libx264='mp4')  # video formats
+#                filespec = 'movie/%1dtmp_%04d' %movieCounter + outfileName + '.png'
+#                movie_program = 'ffmpeg'  # or 'avconv'
+#                for codec in codec2ext:
+#                    ext = codec2ext[codec]
+#                    cmd = '%(movie_program)s -r %(fps)d -i %(filespec)s ' \
+#                          '-vcodec %(codec)s movie3_6.%(ext)s' % vars()
+#                    os.system(cmd)
         
     
     
